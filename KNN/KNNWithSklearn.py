5cf88b216c99c7be0c27fc0f772358520525cc72
Ishaan Agarwal <73911978+IshaanA-projects@users.noreply.github.com>
2025-10-26 14:57:24 +0000

    Refactor KNNWithSklearn.py for cleaner code

--- KNN/KNNWithSklearn.py
+++ KNN/KNNWithSklearn.py
@@ -3,7 +3,7 @@ import pandas as pd
 from sklearn import preprocessing, neighbors
 from sklearn.model_selection import train_test_split
 
-df = pd.read_csv(r"C:\Users\Poonam\Documents\Projects\Machine learning\Machine learning with python\KNN\breast-cancer-wisconsin.data")
+df = pd.read_csv("breast-cancer-wisconsin.data")
 df.replace("?", -99999, inplace = True) # Replaces missing data with an outlier that has minimal effect
 df.drop(["id"], axis = 1, inplace = True) # Removes the id column as this is irrelevant to the class
 
@@ -23,5 +23,6 @@ print(f"The accuracy of the algorithm is {accuracy}")
 example_measures = np.array([[4, 2, 1, 1, 1, 2, 3, 2, 1], [4, 1, 1, 3, 2, 1, 3, 1, 1]]) # Example data created to test model's prediction
 example_measures= example_measures.reshape(len(example_measures), -1)
 example_measures = preprocessing.scale(example_measures)
+
 
 print(f"The prediction on our example data is {clf.predict(example_measures)}")
